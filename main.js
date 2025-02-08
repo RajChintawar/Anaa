215edcf039091a331cdd91e0cc4953608cbcce1d
Raxxxx <161466309+RajChintawar@users.noreply.github.com>
2025-02-08 16:39:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+
+const nobtn = document.getElementById("No")
+function fx()
+{
+  const x = Math.floor(Math.random()*300)+1
+  const y = Math.floor(Math.random()*300)+1
+  nobtn.style.top = x+"px"
+  nobtn.style.left = y+"px"
+
+}
